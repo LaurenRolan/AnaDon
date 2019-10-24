48b0a15877be32a1283248994218043ddd9d6e06
Lauren Silva Rolan Sampaio <21904824@campus.unicaen.fr>
2019-10-24 15:34:07 +0200

    Starting TP3

--- TP3/Chap11.R
+++ TP3/Chap11.R
@@ -2,3 +2,25 @@
 ### CHAPITRE 11
 ###
 
+## Application 1
+x = c(6.2, 6.6, 7.1, 7.4, 7.6, 7.9, 8, 8.3, 8.4, 8.5, 
+      8.6, 8.8, 8.8, 9.1, 9.2, 9.4, 9.4, 9.7, 9.9, 10.2,
+      10.4, 10.8, 11.3, 11.9)
+mean(x); var(x)
+qqnorm(x); qqline(x)
+t.test(x, conf.level = 0.95)$conf.int
+library(OneTwoSamples)
+interval_var1(x, alpha = 0.05)[c(3, 4)]
+
+## Application 2
+x = c(54.40, 57.26, 51.53, 55.84, 53.56, 58.44, 54.92, 
+      55.65, 52.53, 50.90, 58.93, 56.30, 57.87, 55.14,
+      56.31, 55.09, 58.02, 56.20, 57.49, 54.89)
+t.test(x, conf.level = 0.99)$conf.int
+
+## Application 3
+x = c(0.499, 0.509, 0.501, 0.494, 0.498, 0.497, 0.504, 
+      0.506, 0.502, 0.496)
+qqnorm(x); qqline(x)
+t.test(x, conf.level = 0.95)
+interval_var1(x, alpha = 0.01)
---
+++ TP3/IntervallesConfiance.R
@@ -0,0 +1,35 @@
+##################
+### Exercice 1 ###
+##################
+
+jeu1 = c(14.6, 13.4, 13.8, 12.9, 12.3, 10.7, 12.9, 16.7, 
+         13.4, 10.4, 14.4, 11.3)
+jeu2 = c(0.32, 1.02, 0.89, 0.24, 0.18, 0.51, 0.14, 1.86, 
+         0.12, 0.26)
+jeu3 = c(1, 2, 2, 3, 6, 1, 1, 2, 3, 2, 1, 2)
+
+par(mfrow = c(1, 3))
+qqnorm(jeu1); qqline(jeu1)
+qqnorm(jeu2); qqline(jeu2)
+qqnorm(jeu3); qqline(jeu3)
+
+## Parmi ceux-ci : hypothèse de normalité ?
+## Seulement le jeu1
+
+##################
+### Exercice 2 ###
+##################
+anonces = c(26, 33, 23, 30, 24, 36, 27, 29, 22, 28, 37, 31, 34, 28, 31,
+            28, 35, 30, 33, 27, 32, 39, 28, 33, 32, 28, 31, 30, 11, 37)
+par(mfrow = c(1, 1))
+qqnorm(anonces); qqline(anonces)
+mean(anonces); sd(anonces)
+t.test(anonces, conf.level = 0.95)$conf.int
+interval_var1(x, alpha = 0.05)[c(3,4)]
+
+##################
+### Exercice 3 ###
+##################
+x = c(rep(1612.5, 6), rep(1617.5, 9), rep(1622.5, 46), rep(1627.5, 78), rep(1632.5, 21))
+mean(x); sd(x)
+t.test(x, conf.level = 0.95)$conf.int
